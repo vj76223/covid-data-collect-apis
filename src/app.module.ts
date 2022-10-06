2d7c92fc4aca0bd7b993c39aa8f8bbe9207e63b4
jhavinit <jhavinit07@gmail.com>
2022-10-06 17:09:09 +0530

    completed doctor CRUD operations

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PatientModule } from './patient/patient.module';
+import { DoctorModule } from './doctor/doctor.module';
 import { PatientModule } from './patient/patient.module';
 
 @Module({
-  imports: [PatientModule],
+  imports: [PatientModule, DoctorModule],
   controllers: [AppController],
   providers: [AppService],
 })
