80e97368937eaf984addb5fdc6449fbc047639de
jhavinit <jhavinit07@gmail.com>
2022-10-06 17:27:38 +0530

    edited apis

--- src/redis-cache-mod/dto/update-redis-cache-mod.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateRedisCacheModDto } from './create-redis-cache-mod.dto';
-
-export class UpdateRedisCacheModDto extends PartialType(CreateRedisCacheModDto) {
-  id: number;
-}
--- src/redis-cache-mod/redis-cache-mod.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RedisCacheModService } from './redis-cache-mod.service';
-import { RedisCacheModController } from './redis-cache-mod.controller';
-
-@Module({
-  controllers: [RedisCacheModController],
-  providers: [RedisCacheModService]
-})
-export class RedisCacheModModule {}
